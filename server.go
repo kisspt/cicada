2122e1cba6a9df62300f324ebb4463e1a4c4b02d
kisspt <PanT1221@1221.com>
2022-08-30 20:03:25 +0800

    update

--- server.go
+++ server.go
@@ -1,50 +1,21 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-
-	"github.com/gorilla/websocket"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
-var upgarder = websocket.Upgrader{
-	CheckOrigin: func(r *http.Request) bool {
-		return true
-	},
-}
-
-func StartServer(c echo.Context) error {
-	ws, err := upgarder.Upgrade(c.Response(), c.Request(), nil)
-	if err != nil {
-		return err
-	}
-	defer ws.Close()
-
-	id := c.Param("id")
-	fmt.Printf("%s", id)
-
-	for {
-		err := ws.WriteJSON(map[string]interface{}{"title": "Ant Design Title"})
-		// err := ws.WriteMessage(websocket.TextMessage, []byte("Hello World!"))
-		if err != nil {
-			c.Logger().Error(err)
-		}
-
-		_, msg, err := ws.ReadMessage()
-		if err != nil {
-			c.Logger().Error(err)
-		}
-		fmt.Printf("%s\n", msg)
-	}
-}
-
 func main() {
+	hub := newHub()
+	go hub.run()
+
 	e := echo.New()
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 	e.Static("/", "../public")
-	e.GET("/ws", StartServer)
+	e.GET("/ws", func(c echo.Context) error {
+		err := serveWs(hub, c)
+		return err
+	})
 	e.Logger.Fatal(e.Start(":1323"))
 }
